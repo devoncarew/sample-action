ed851ea08350f0e676f3210337cc893ea58ec8a6
Devon Carew <devoncarew@gmail.com>
2023-02-16 14:19:46 -0800

    fix raw flavor

--- src/main.ts
+++ src/main.ts
@@ -28,6 +28,7 @@ async function run(): Promise<void> {
       core.setFailed(`Unrecognized build flavor '${flavor}'.`)
       return
     }
+    const raw = flavor === 'raw'
 
     // os
     const os: string = system.getPlatform()
@@ -44,10 +45,14 @@ async function run(): Promise<void> {
 
     if (sdk === 'stable' || sdk === 'beta' || sdk === 'dev') {
       channel = sdk
-      version = (await versions.getLatestVersion(channel, flavor)) as string
+      version = raw
+        ? 'latest'
+        : ((await versions.getLatestVersion(channel, flavor)) as string)
     } else if (sdk === 'main') {
       channel = 'be'
-      version = (await versions.getLatestVersion(channel, flavor)) as string
+      version = raw
+        ? 'latest'
+        : ((await versions.getLatestVersion(channel, flavor)) as string)
     } else {
       version = sdk
 
@@ -66,8 +71,13 @@ async function run(): Promise<void> {
 
     core.info(
       `Installing the ${os}-${architecture} Dart SDK version ${version} from ` +
-        `the ${channel} (${flavor}) channel.`
+      `the ${channel} (${flavor}) channel.`
     )
+
+    // should be:
+    // https://storage.googleapis.com/dart-archive/channels/be/raw/latest/sdk/dartsdk-linux-x64-release.zip...
+    // is:
+    // https://storage.googleapis.com/dart-archive/channels/be/raw/3.0.0-edge.71bbeddf0004f1d1992fbc54d56aa99b4f95cb0a/sdk/dartsdk-linux-x64-release.zip
 
     // Calculate url based on https://dart.dev/tools/sdk/archive#download-urls.
     const url =
@@ -76,8 +86,10 @@ async function run(): Promise<void> {
       `dartsdk-${os}-${architecture}-release.zip`
 
     // use cached sdk, or download and cache the sdk
-    const toolName = flavor === 'raw' ? 'dart-be' : 'dart'
-    let sdkPath = tc.find(toolName, version, architecture)
+    const toolName = flavor === 'raw' ? 'dart_raw' : 'dart'
+
+    // Using a 'raw' sdk flavor disables caching.
+    let sdkPath = !raw ? tc.find(toolName, version, architecture) : null
     if (sdkPath) {
       core.info(`Using cached sdk from ${sdkPath}.`)
     } else {
